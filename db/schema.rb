23c78299f2fcd94b92e6e186f1b61ab6ec83a760
Ryan Ore <ryanore@jellyvision.com>
2016-11-25 16:21:49 -0600

    Update associations and add seed after dropping everything

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161125140356) do
+ActiveRecord::Schema.define(version: 20161125201407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id", using: :btree
   end
 
   create_table "movies", force: :cascade do |t|
+    t.jsonb    "data"
+    t.integer  "list_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_movies_on_list_id", using: :btree
+    t.index ["user_id"], name: "index_movies_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "access_token"
     t.string   "password_digest"
     t.string   "role"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "lists", "users"
+  add_foreign_key "movies", "lists"
+  add_foreign_key "movies", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,80 @@
 #
 # Examples:
 #
-User.create!(email: 'ryan@ryanore.com', first_name: 'Ryan', last_name: 'Ore', password: '123456',  role: 'admin')
+User.create!(email: 'ryan@ryanore.com', first_name: 'Ryan', last_name: 'Admin', password: '123456',  role: 'admin')
+
+User.create!(email: 'ryanoremail@gmail.com', first_name: 'RyanUser', last_name: 'User', password: '123456',  role: 'user')
+
+List.create!(user_id: '1', name: 'Documentaries(admin)' )
+
+List.create!(user_id: '2', name: 'Documentaries(user)' )
+
+Movie.create!([
+  {
+    user_id: '1',
+    list_id: '1',
+    data: {
+      "poster_path": "/ub8d4LNtfwfibUclk89KDNQJx5k.jpg",
+      "adult": false,
+      "overview": "Eddie Izzard's routine has a loose trajectory from the beginning of the Old Testament and the creation of the world in seven days to Revelations. Along the way, we learn of the search for a career, bad giraffes, Prince Philip's gaffes, toilets in French campsites, the mysteries of hopscotch, becoming one's Dad and tranny bashing.",
+      "release_date": "1997-11-17",
+      "genre_ids": [
+      35,
+      99
+      ],
+      "id": 13590,
+      "original_title": "Eddie Izzard: Glorious",
+      "original_language": "en",
+      "title": "Eddie Izzard: Glorious",
+      "backdrop_path": "/2XHkh7xLqH168KRqMwDuiTmuyQi.jpg",
+      "popularity": 5.240644,
+      "vote_count": 9,
+      "video": false,
+      "vote_average": 5.67
+    }
+  } , {
+    user_id: '2',
+    list_id: '2',
+    data: {
+      "poster_path": "/ub8d4LNtfwfibUclk89KDNQJx5k.jpg",
+      "adult": false,
+      "overview": "Eddie Izzard's routine has a loose trajectory from the beginning of the Old Testament and the creation of the world in seven days to Revelations. Along the way, we learn of the search for a career, bad giraffes, Prince Philip's gaffes, toilets in French campsites, the mysteries of hopscotch, becoming one's Dad and tranny bashing.",
+      "release_date": "1997-11-17",
+      "genre_ids": [
+      35,
+      99
+      ],
+      "id": 13590,
+      "original_title": "Eddie Izzard: Glorious",
+      "original_language": "en",
+      "title": "Eddie Izzard: Glorious",
+      "backdrop_path": "/2XHkh7xLqH168KRqMwDuiTmuyQi.jpg",
+      "popularity": 5.240644,
+      "vote_count": 9,
+      "video": false,
+      "vote_average": 5.67
+    }
+  } , {
+    user_id: '1',
+    list_id: '1',
+    data: {
+      "poster_path": "/ub8d4LNtfwfibUclk89KDNQJx5k.jpg",
+      "adult": false,
+      "overview": "Eddie Izzard's routine has a loose trajectory from the beginning of the Old Testament and the creation of the world in seven days to Revelations. Along the way, we learn of the search for a career, bad giraffes, Prince Philip's gaffes, toilets in French campsites, the mysteries of hopscotch, becoming one's Dad and tranny bashing.",
+      "release_date": "1997-11-17",
+      "genre_ids": [
+      35,
+      99
+      ],
+      "id": 13590,
+      "original_title": "Eddie Izzard: Glorious",
+      "original_language": "en",
+      "title": "Eddie Izzard: Glorious",
+      "backdrop_path": "/2XHkh7xLqH168KRqMwDuiTmuyQi.jpg",
+      "popularity": 5.240644,
+      "vote_count": 9,
+      "video": false,
+      "vote_average": 5.67
+    }
+  }
+])
